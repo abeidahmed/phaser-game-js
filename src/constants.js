c7c6187fa53a19d696369f1627c82b0ad431df98
abeidahmed <abeidahmed92@gmail.com>
2021-03-20 13:00:25 +0530

    Create game constants

---
+++ src/constants.js
@@ -0,0 +1,10 @@
+export const GRAVITY = 800;
+export const MAX_UPWARDS_VELOCITY = -300;
+export const UPWARDS_ACCELERATION = -450;
+export const TERMINAL_VELOCITY = 400;
+export const PIPE_SPACING_Y = 100;
+export const TREADMILL_SPEED = -125;
+
+export const CONFIG_WIDTH = 960;
+export const CONFIG_HEIGHT = 540;
+export const GROUND_Y = CONFIG_HEIGHT;
