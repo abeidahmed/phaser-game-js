94d2431b36a4ab2b94343e96e350d47ec34328a7
abeidahmed <abeidahmed92@gmail.com>
2021-03-20 13:50:37 +0530

    Extract game constants

--- src/constants.js
+++ src/constants.js
@@ -5,6 +5,6 @@ export const TERMINAL_VELOCITY = 400;
 export const PIPE_SPACING_Y = 100;
 export const TREADMILL_SPEED = -125;
 
-export const CONFIG_WIDTH = 960;
-export const CONFIG_HEIGHT = 540;
+export const CONFIG_WIDTH = document.documentElement.clientWidth;
+export const CONFIG_HEIGHT = document.documentElement.clientHeight;
 export const GROUND_Y = CONFIG_HEIGHT;
