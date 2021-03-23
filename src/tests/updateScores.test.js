3bb516af21afe91d3424cd63a208846c6d94b4bb
abeidahmed <abeidahmed92@gmail.com>
2021-03-23 14:38:26 +0530

    Mock tests using jest-fetch-mock lib

--- src/tests/updateScores.test.js
+++
@@ -1,8 +0,0 @@
-import updateScores from './mocks/updateScores.mock';
-
-test('it updates the score of the user successfully', () => {
-  const data = updateScores({ name: 'John', score: '10' });
-  data.then((res) => {
-    expect(res).toMatch(/score created/);
-  });
-});
